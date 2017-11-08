5a0992419b3bd5e2c7ef05cd79ffb7bd299bf755
aperov <perov@reksoft.ru>
2017-11-08 18:21:45 +0300

    Add design pattern <Singleton>

---
+++ Singleton/Singleton.js
@@ -0,0 +1,31 @@
+const Singelton = (function() {
+    let instance;
+  
+    function createInstance() {
+      return {
+        identificator: Math.floor(Math.random() * 1000000),
+        page: null,
+        getId: function() {
+          return this.id;
+        },
+        setId: function(id) {
+          this.id = id;
+          return true;
+        }
+      }
+    }
+  
+    return function() {
+        if(!instance) { 
+          instance = createInstance();
+        }
+        return instance;
+    }
+    
+    
+})();
+
+console.log(new Singelton().setId(3));
+console.log(new Singelton().setId(10));
+console.log(new Singelton());
+console.log(new Singelton());
