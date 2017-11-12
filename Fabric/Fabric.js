f69686c640d58f6a0dbf1705132e527c7ece1964
snaipi91 <snaipi91@rambler.ru>
2017-11-12 22:48:35 +0300

    Fabric

--- Fabric/Fabric.js
+++ Fabric/Fabric.js
@@ -1,28 +1,43 @@
-var root = document.getElementById('root');
+const root = document.getElementById('root');
 
-function FabricButton(options) {
+const FabricButton = function(options) {
   
-    var element = document.createElement('button');
-    
-    function init() {
-      element.innerHTML = (typeof options.text == 'string') ? options.text : 'there had to be text';
-      element.style.color = (typeof options.color == 'string') ? options.color : 'blue';
-    }
+    this.element = document.createElement('button');
+    this.element.innerHTML = (typeof options.text == 'string') ? options.text : 'there had to be text';
+    this.element.style.color = (typeof options.color == 'string') ? options.color : 'blue';
     
     FabricButton.prototype.render = function() {
-        init();
-        root.appendChild(element);
-        console.log(element);
+        root.appendChild(this.element);
     }
 }
 
-function FabricButtonPopup() {
-    
+const FabricButtonPopup = function(options) { 
+    FabricButton.call(this, options);
+    this.element.addEventListener('click', this.popup);
 }
 
-var button = new FabricButton({
-  text: 'press here',
+FabricButtonPopup.prototype = Object.create(FabricButton.prototype);
+FabricButtonPopup.prototype.constructor = FabricButtonPopup;
+
+FabricButtonPopup.prototype.popup = function() {
+  prompt('open window')
+}
+
+let button = new FabricButton({
+  text: 'press here man',
   color: null
 })
 
+let buttonPopup = new FabricButtonPopup({
+  text: 'press here man',
+  color: 'red'
+})
+
+let buttonPopupNew = new FabricButtonPopup({
+  text: 'press here man',
+  color: 'green'
+})
+
 button.render()
+buttonPopup.render()
+buttonPopupNew.render()
